b24e43656c7566da34606bb98119737a4a2d3260
THtianhao <356831631@qq.com>
2017-04-09 01:41:03 +0800

    美化UI

--- app/src/main/java/com/example/toto/aiapp/MainActivity.java
+++ app/src/main/java/com/example/toto/aiapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -24,11 +25,7 @@ import java.util.Map;
 
 public class MainActivity extends Activity implements View.OnClickListener {
     private static final String TAG = "MainActivity";
-    private Button textDemoButton;
-    private Button speechDemoButton;
-    private Button mTotobutton;
-    private RequestQueue requestQueue;
-    private String mUrl = "http://10.60.233.41:8080/";
+    private TextView mEnter;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -48,35 +45,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
      */
     private void init() {
         Log.e(TAG, "Initializing app");
-        textDemoButton = (Button) findViewById(R.id.button_select_text);
-        speechDemoButton = (Button) findViewById(R.id.button_select_voice);
-        mTotobutton = (Button) findViewById(R.id.totobutton);
-        mTotobutton.setOnClickListener(this);
-        textDemoButton.setOnClickListener(this);
-        speechDemoButton.setOnClickListener(this);
-
+        mEnter = (TextView) findViewById(R.id.enter);
+        mEnter.setOnClickListener(this);
     }
-
-    StringRequest stringRequest = new StringRequest(Request.Method.POST, mUrl, new Response.Listener<String>() {
-        @Override
-        public void onResponse(String response) {
-            Log.d("tianhao", "success");
-        }
-    }, new Response.ErrorListener() {
-        @Override
-        public void onErrorResponse(VolleyError error) {
-            Log.d("tianhao", "fail");
-        }
-    }) {
-        @Override
-        protected Map<String, String> getParams() throws AuthFailureError {
-            HashMap<String, String> mMap = new HashMap<>();
-            mMap.put("param", "toto");
-            return mMap;
-        }
-    };
-
-
     /**
      * On-click listener for buttons text and voice buttons.
      *
@@ -86,19 +57,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
     @Override
     public void onClick(final View v) {
         switch ((v.getId())) {
-            case R.id.button_select_text:
+            case R.id.enter:
                 Intent textIntent = new Intent(this, TextActivity.class);
                 startActivity(textIntent);
-                break;
-            case R.id.button_select_voice:
-                Intent voiceIntent = new Intent(this, InteractiveVoiceActivity.class);
-                startActivity(voiceIntent);
-                break;
-            case R.id.totobutton:
-                requestQueue.add(stringRequest);
-                Intent newIntent = new Intent(this, CostomActivity.class);
-                newIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(newIntent);
                 break;
         }
     }
